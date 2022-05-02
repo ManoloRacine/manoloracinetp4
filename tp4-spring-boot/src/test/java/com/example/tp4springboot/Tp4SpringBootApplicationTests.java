27255f985b4a2bcb97e9e39ae90a991299cca341
ManoloRacine <manolo191202@gmail.com>
2022-05-02 01:26:43 -0400

    création du projet react et du projet spring boot

---
+++ tp4-spring-boot/src/test/java/com/example/tp4springboot/Tp4SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp4springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
